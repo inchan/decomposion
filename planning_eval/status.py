6cdbbe7310cb97a33e330a0f9f62c45cf3a452b2
Kay <kangsazang@gmail.com>
2026-09-16 11:10:56 +0900

    feat: expose Planning Eval Step 1 capability status

---
+++ planning_eval/status.py
@@ -0,0 +1,11 @@
+CAPABILITIES = {
+    "reference_constraint_model": True,
+    "deterministic_selftests": True,
+    "semantic_match_contract": True,
+    "human_adjudication_contract": True,
+    "multidimensional_scorecard": True,
+    "real_semantic_matcher": False,
+    "judge_calibrated": False,
+    "deep_reference_cases": False,
+    "visual_review_eval": False,
+}
