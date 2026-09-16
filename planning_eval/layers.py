54547f3dfd3c50d449eacab6ba8b325312032a15
Kay <kangsazang@gmail.com>
2026-09-16 11:11:23 +0900

    feat: define Planning Eval responsibility layers

---
+++ planning_eval/layers.py
@@ -0,0 +1,20 @@
+DETERMINISTIC_RESPONSIBILITIES = (
+    "schema_validity",
+    "duplicate_ids",
+    "dangling_edges",
+    "hard_dependency_cycles",
+    "explicit_traceability_structure",
+)
+
+SEMANTIC_RESPONSIBILITIES = (
+    "concept_equivalence",
+    "candidate_to_reference_matching",
+    "semantic_dependency_equivalence",
+)
+
+HUMAN_RESPONSIBILITIES = (
+    "ambiguous_granularity",
+    "contested_severity",
+    "novel_finding_adjudication",
+    "ambiguous_dependency",
+)
